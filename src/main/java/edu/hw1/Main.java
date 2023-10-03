a098ae278e540594986375b43e5bc66520646c48
A1 <portnovivan2011@mail.ru>
2023-10-03 14:07:08 +0300

    Added log4j2.xml and Logger

---
+++ src/main/java/edu/hw1/Main.java
@@ -0,0 +1,11 @@
+package edu.hw1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static void main(String[] args) {
+        LOGGER.info("Привет мир!");
+    }
+}
