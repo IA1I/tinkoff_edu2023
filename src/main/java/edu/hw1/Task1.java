2b51a30fc77987ce5dae77321ffc805d9e14169b
A1 <portnovivan2011@mail.ru>
2023-10-07 17:09:30 +0300

    Added implementation of Task1 - minutesToSeconds

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -1,8 +1,34 @@
 package edu.hw1;
 
 public class Task1 {
+    public static final String COLON = ":";
+    public static final int ARRAY_SIZE = 2;
+    public static final int WRONG_ANSWER = -1;
+    public static final int SIXTY_SECONDS = 60;
     public static int minutesToSeconds(String videoLength){
+        int durationInSeconds;
+        int[] duration;
+        try {
+            duration = inputProcessing(videoLength);
+            durationInSeconds = duration[0]*SIXTY_SECONDS + duration[1];
+        } catch (Exception e){
+            durationInSeconds = WRONG_ANSWER;
+        }
 
-        return 0;
+
+        return durationInSeconds;
+    }
+
+    private static int[] inputProcessing(String videoLength) throws Exception {
+        String[] input = videoLength.split(COLON);
+        int[] duration = new int[ARRAY_SIZE];
+        for(int i = 0; i < ARRAY_SIZE; i++){
+            duration[i] = Integer.parseInt(input[i]);
+        }
+        if(duration[1] >= SIXTY_SECONDS){
+            throw new Exception();
+        }
+
+        return duration;
     }
 }
