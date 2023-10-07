c68d47e69f91d3b60d196b2b7aa48bf41d1a08fb
A1 <portnovivan2011@mail.ru>
2023-10-07 19:52:12 +0300

    Added implementation of Task3 - isNestabe

--- src/main/java/edu/hw1/Task3.java
+++ src/main/java/edu/hw1/Task3.java
@@ -1,7 +1,39 @@
 package edu.hw1;
 
+import java.util.Arrays;
+
 public class Task3 {
+    public static final int ZERO_LENGTH = 0;
     public static boolean isNestable(int[] firstArray, int[] secondArray){
-        return false;
+        if(!isValidInput(firstArray, secondArray)){
+            return false;
+        }
+        int firstMin = Integer.MAX_VALUE, secondMin = Integer.MAX_VALUE;
+        int firstMax = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
+        for(var number : firstArray){
+            firstMin = Math.min(firstMin, number);
+            firstMax = Math.max(firstMax, number);
+        }
+        for(var number : secondArray){
+            secondMin = Math.min(secondMin, number);
+            secondMax = Math.max(secondMax, number);
+        }
+
+        return firstMin > secondMin && firstMax < secondMax;
+    }
+
+    private static boolean isValidInput(int[] firstArray, int[] secondArray){
+        if(firstArray == null || secondArray == null){
+            return false;
+        }
+        return firstArray.length != ZERO_LENGTH && secondArray.length != ZERO_LENGTH;
+    }
+    private static int findMin(int[] array){
+        int min = Integer.MAX_VALUE;
+        for(var number : array){
+            min = Math.min(min, number);
+        }
+
+        return min;
     }
 }
