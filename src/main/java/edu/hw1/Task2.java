6352e47ef8f2ffb7964e6826ae987c80cebf901c
A1 <portnovivan2011@mail.ru>
2023-10-07 18:57:16 +0300

    Added tests for Task2

---
+++ src/main/java/edu/hw1/Task2.java
@@ -0,0 +1,9 @@
+package edu.hw1;
+
+public class Task2 {
+    public static int countDigits(int number){
+        int numberOfDigits = 0;
+
+        return numberOfDigits;
+    }
+}
--- src/test/java/edu/hw1/SampleTest.java
+++ src/test/java/edu/hw1/SampleTest.java
@@ -3,13 +3,17 @@ package edu.hw1;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SampleTest {
 
 
     @Nested
+    @DisplayName("1. Длина видео")
     class TestTask1{
         @ParameterizedTest
         @DisplayName("Перевод из минут в секунды")
@@ -45,6 +49,33 @@ public class SampleTest {
             int actual = Task1.minutesToSeconds(input);
 
             assertThat(actual).isEqualTo(expected);
+        }
+    }
+
+    @Nested
+    @DisplayName("2. Количество цифр")
+    class TestTask2{
+        @ParameterizedTest
+        @MethodSource("testCases")
+        void testRegularInput(final int input, final int expected){
+            int actual = Task2.countDigits(input);
+
+            assertThat(actual).isEqualTo(expected);
+        }
+        static Stream<Arguments> testCases(){
+            return Stream.of(
+                Arguments.of(12345, 5),
+                Arguments.of(1234, 4),
+                Arguments.of(123, 3),
+                Arguments.of(12, 2),
+                Arguments.of(1, 1),
+                Arguments.of(0, 1),
+                Arguments.of(-1, 1),
+                Arguments.of(-12, 2),
+                Arguments.of(-123, 3),
+                Arguments.of(-1234, 4),
+                Arguments.of(-12345, 5)
+            );
         }
     }
 }
