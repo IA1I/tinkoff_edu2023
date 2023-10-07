0d410f645b6180f99dccea39a8610e4cdb99a284
A1 <portnovivan2011@mail.ru>
2023-10-07 20:30:54 +0300

    Added implementation of Task4 - fixString

--- src/main/java/edu/hw1/Task4.java
+++ src/main/java/edu/hw1/Task4.java
@@ -5,6 +5,13 @@ public class Task4 {
         if(incorrectString == null){
             throw new IllegalArgumentException();
         }
-        return null;
+        StringBuffer correctString = new StringBuffer(incorrectString);
+        for(int i = 0; i < correctString.length()-1; i+=2){
+            char tmp = correctString.charAt(i);
+            correctString.setCharAt(i, correctString.charAt(i+1));
+            correctString.setCharAt(i+1, tmp);
+        }
+
+        return correctString.toString();
     }
 }
