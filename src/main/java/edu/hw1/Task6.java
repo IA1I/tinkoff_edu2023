03f76bf5d7e2dc34df6309071c826412aca3666f
A1 <portnovivan2011@mail.ru>
2023-10-09 14:38:28 +0300

    Added implementation of Task7 - Cyclic bit shift

--- src/main/java/edu/hw1/Task6.java
+++ src/main/java/edu/hw1/Task6.java
@@ -1,7 +1,6 @@
 package edu.hw1;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,17 +10,18 @@ public class Task6 {
     public static final int WRONG_NUMBER_OF_DIGITS = 1;
     public static final int KAPREKAR_CONST = 6174;
 
-    private Task6(){}
+    private Task6() {
+    }
 
-    public static int countK(int number){
+    public static int countK(int number) {
         checkInput(number);
         int countK = recursiveCountK(number, 0);
 
         return countK;
     }
 
-    private static void checkInput(int number){
-        if(number <= BOTTOM_LIMIT || number >= UPPER_LIMIT){
+    private static void checkInput(int number) {
+        if (number <= BOTTOM_LIMIT || number >= UPPER_LIMIT) {
             throw new IllegalArgumentException();
         }
         char[] stringNumber = String.valueOf(number).toCharArray();
@@ -29,30 +29,31 @@ public class Task6 {
         for (char c : stringNumber) {
             digits.add(c);
         }
-        if(digits.size() == WRONG_NUMBER_OF_DIGITS){
+        if (digits.size() == WRONG_NUMBER_OF_DIGITS) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static int recursiveCountK(int number, int countK){
-        if(number == KAPREKAR_CONST){
+    private static int recursiveCountK(int number, int countK) {
+        if (number == KAPREKAR_CONST) {
             return countK;
         }
         int maxNumber = getMaxNumber(number);
         int minNumber = getMinNumber(number);
 
-        return recursiveCountK(maxNumber-minNumber, countK+1);
+        return recursiveCountK(maxNumber - minNumber, countK + 1);
     }
 
-    private static int getMaxNumber(int number){
-        int[] arrayNumber = new int[]{number%10, number/10%10, number/100%10, number/1000};
+    private static int getMaxNumber(int number) {
+        int[] arrayNumber = new int[] {number % 10, number / 10 % 10, number / 100 % 10, number / 1000};
         Arrays.sort(arrayNumber);
         int maxNumber = arrayNumber[3] * 1000 + arrayNumber[2] * 100 + arrayNumber[1] * 10 + arrayNumber[0];
 
         return maxNumber;
     }
-    private static int getMinNumber(int number){
-        int[] arrayNumber = new int[]{number%10, number/10%10, number/100%10, number/1000};
+
+    private static int getMinNumber(int number) {
+        int[] arrayNumber = new int[] {number % 10, number / 10 % 10, number / 100 % 10, number / 1000};
         Arrays.sort(arrayNumber);
         int minNumber = arrayNumber[0] * 1000 + arrayNumber[1] * 100 + arrayNumber[2] * 10 + arrayNumber[3];
 
---
+++ src/main/java/edu/hw1/Task7.java
@@ -0,0 +1,40 @@
+package edu.hw1;
+
+public class Task7 {
+    public static final int ZERO = 0;
+    public static final int RADIX = 2;
+
+    public static int rotateLeft(int number, int shift) {
+        checkInput(number, shift);
+        char[] binaryNumber = convertToBinary(number);
+        final int NUMBER_LENGTH = binaryNumber.length;
+        char[] shiftedBinaryNumber = new char[NUMBER_LENGTH];
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            shiftedBinaryNumber[i] = binaryNumber[(i + shift) % NUMBER_LENGTH];
+        }
+
+        return Integer.parseInt(String.valueOf(shiftedBinaryNumber), RADIX);
+    }
+
+    public static int rotateRight(int number, int shift) {
+        checkInput(number, shift);
+        char[] binaryNumber = convertToBinary(number);
+        final int NUMBER_LENGTH = binaryNumber.length;
+        char[] shiftedBinaryNumber = new char[NUMBER_LENGTH];
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            shiftedBinaryNumber[(i + shift) % NUMBER_LENGTH] = binaryNumber[i];
+        }
+
+        return Integer.parseInt(String.valueOf(shiftedBinaryNumber), RADIX);
+    }
+
+    private static char[] convertToBinary(int number) {
+        return Integer.toBinaryString(number).toCharArray();
+    }
+
+    private static void checkInput(int number, int shift) {
+        if (number < ZERO || shift < ZERO) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
