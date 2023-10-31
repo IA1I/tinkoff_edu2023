e943aa71a9e663c87a7c0b75568843444abec0e3
A1 <portnovivan2011@mail.ru>
2023-10-31 22:09:31 +0300

    Fixed for checkstyle

--- src/main/java/edu/hw4/AnimalUtils.java
+++ src/main/java/edu/hw4/AnimalUtils.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
