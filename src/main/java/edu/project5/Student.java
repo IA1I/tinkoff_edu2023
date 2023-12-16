fe7b655c69c5d02c222f51c8d1cba8359eb9a0cf
A1 <portnovivan2011@mail.ru>
2023-12-16 16:33:43 +0300

    Added tests and implementation for project5

---
+++ src/main/java/edu/project5/Student.java
@@ -0,0 +1,13 @@
+package edu.project5;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
